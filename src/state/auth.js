0c24f624097a0acf889a6539c54eb2e757025f73
mrozo2m <mrozo2m@interia.pl>
2019-04-18 14:10:19 +0200

    startListeningUserLoginsLogsAsyncActionCreator

--- src/state/auth.js
+++ src/state/auth.js
@@ -3,73 +3,101 @@ import { auth, googleProvider, database } from '../firebaseConf'
 const EMAIL_CHANGED = 'auth/EMAIL_CHANGED'
 const PASS_CHANGED = 'auth/PASS_CHANGED'
 const SET_USER = 'auth/SET_USER'
+const SET_USER_LOGINS_LOGS = 'auth/SET_USER_LOGINS_LOGS'
 
 export const startListeningToAuthChangesAsyncActionCreator = (
     () => (dispatch, getState) => {
         auth.onAuthStateChanged(
             (user) => {
-                console.log(user)
-                dispatch(setUserActionCreator(user))
+                console.log(user) 
 
                 if (user) {
+                    // USER LOGGED IN
+                    dispatch(setUserActionCreator(user))
                     dispatch(logUserLoginsAsyncActionCreator())
+                    dispatch(startListeningUserLoginsLogsAsyncActionCreator())
                 } else {
-                    //USER NOT LOGGED IN
+                    // USER NOT LOGGED IN
+                    dispatch(setUserActionCreator(user))
                 }
             }
         )
     }
-
 )
 
+export const stopListeningUserLoginsLogsAsyncActionCreator =(
+    () => (dispatch, getState) => {
+        const state = getState()
+        const userId = state.auth.user.uid
+
+        database.ref(`jfddl7/users/${userId}/login`).off()
+    }    
+)
+
+export const startListeningUserLoginsLogsAsyncActionCreator = (
+    () => (dispatch, getState) => {
+        const state = getState()
+        const userId = state.auth.user.uid
+
+        database.ref(`jfddl7/users/${userId}/login`)
+            .on(
+                'value',
+                (snapshot) => {
+                    dispatch(
+                        setUserLoginsLogsActionCreator(
+                            snapshot.val()
+                        )
+                    )
+                }
+            )
+    }
+)
 export const logUserLoginsAsyncActionCreator = () => (dispatch, getState) => {
     const state = getState()
     const userId = state.auth.user.uid
 
     database.ref(`jfddl7/users/${userId}/login`)
         .push({
-            tomestamp: Date.now(),
+            timestamp: Date.now(),
         })
 }
-
 export const logInAsyncActionCreator = () => (dispatch, getState) => {
     const state = getState()
     const email = state.auth.email
     const password = state.auth.password
 
     auth.signInWithEmailAndPassword(email, password)
-        .then(() => console.log('Zalogowano'))
-        .catch((error) => console.log('Wystapil blad', error))
+        .then(() => console.log('ZALOGOWANO'))
+        .catch((error) => console.log('WYSTĄPIŁ BŁĄD', error))
 }
-
 export const logInByGoogleAsyncActionCreator = () => (dispatch, getState) => {
     auth.signInWithPopup(googleProvider)
-        .then(() => console.log('Zalogowano'))
-        .catch((error) => console.log('Wystapil blad', error))
+        .then(() => console.log('ZALOGOWANO'))
+        .catch((error) => console.log('WYSTĄPIŁ BŁĄD', error))
 }
-
 export const logOut = () => (dispatch, getState) => {
     auth.signOut()
 }
 
+const setUserLoginsLogsActionCreator = data => ({
+    type: SET_USER_LOGINS_LOGS,
+    data
+})
 const setUserActionCreator = user => ({
     type: SET_USER,
     user,
 })
-
 export const emailChangedActionCreator = newValue => ({
     type: EMAIL_CHANGED,
     newValue,
 })
-
 export const passChangedActionCreator = newValue => ({
     type: PASS_CHANGED,
     newValue,
 })
 
-
-
 const initialState = {
+    userLoginsLogs: null,
     user: null,
     email: '',
     password: '',
@@ -91,6 +119,11 @@ export default (state = initialState, action) => {
             return {
                 ...state,
                 user: action.user,
+            }
+        case SET_USER_LOGINS_LOGS:
+            return {
+                ...state,
+                userLoginsLogs: action.data,
             }
         default:
             return state
